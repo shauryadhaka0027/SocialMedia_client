05745f339363eb88bf00a24de28e6ee4322d46d4
shaurya <shauryadhaka3@gmail.com>
2024-12-09 16:03:21 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import { Navbar } from "./Components/Navbar/Navbar";
 import { HomePage } from "./Pages/HomePage/HomePage";
 import MainRoute from "./routes/mainRoute";
 
+
 function App() {
 return<>
 <MainRoute/>
-{/* <Navbar/>
-<HomePage/> */}
 </>
 }
 
