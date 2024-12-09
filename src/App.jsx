c82f408c5a7ff5d51a15927a16e68461957ff169
shaurya <shauryadhaka3@gmail.com>
2024-12-09 16:23:21 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { HomePage } from "./Pages/HomePage/HomePage";
-import MainRoute from "./routes/mainRoute.jsx";
+import MainRoute from "./routes/MainRoute.jsx";
 
 
 function App() {
